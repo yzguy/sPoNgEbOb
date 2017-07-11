95fa088ca2f4fa3cdb609e1f6388c03e12f64f02
Adam Smith <zero1three@gmail.com>
2017-07-11 13:17:18 -0700

    initial

---
+++ main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strings"
+)
+
+var (
+	input = os.Args[1]
+)
+
+func main() {
+	fmt.Println(sPoNgEbOb(input))
+}
+
+func sPoNgEbOb(input string) (output string) {
+	for i, v := range input {
+		s := string(v)
+		if i%2 == 0 {
+			output += strings.ToLower(s)
+		} else {
+			output += strings.ToUpper(s)
+		}
+	}
+
+	return
+}
